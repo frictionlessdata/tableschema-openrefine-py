2b93bab4580f49f4c3c3f59c203a82a6deb9067c
Vitor Baptista <vitor@vitorbaptista.com>
2015-11-17 14:03:25 +0000

    Raise TypeError if the server_url isn't a string

--- jsontableschema/plugins/openrefine/openrefine_client.py
+++ jsontableschema/plugins/openrefine/openrefine_client.py
@@ -1,9 +1,13 @@
 import requests
+import six
 import six.moves.urllib.parse as urlparse
 
 
 class OpenRefineClient(object):
     def __init__(self, server_url):
+        if not isinstance(server_url, six.string_types):
+            raise TypeError('"server_url" must be a string')
+
         self.server_url = server_url
 
     @property
--- tests/plugins/openrefine/test_openrefine_client.py
+++ tests/plugins/openrefine/test_openrefine_client.py
@@ -1,4 +1,5 @@
 import vcr
+import pytest
 import jsontableschema.plugins.openrefine.openrefine_client as openrefine_cli
 
 
@@ -8,6 +9,11 @@ VCR_CASSETTE = 'fixtures/vcr_cassettes/openrefine_client.yaml'
 
 class TestOpenRefineClient(object):
     _DEFAULT_URL = 'http://localhost:3333'
+
+    def test_invalid_url_raises_exception(self):
+        with pytest.raises(TypeError):
+            not_a_string = 1000
+            OpenRefineClient(not_a_string)
 
     @vcr.use_cassette(VCR_CASSETTE)
     def test_recline_version(self):
